0e2014ca9880bceeee6ce5a44a043409ed056fe2
tmr0716 <toshanranade@gmail.com>
2026-03-23 06:33:54 +0530

    ccalculator in c++using if-else

---
+++ Calc if else.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    char op;
+    cout<<"Enter first number: ";
+    cin>>a;
+    cout<<"Choose operator from [+, -, *, /, %]: ";
+    cin>>op;
+    cout<<"Enter second number: ";
+    cin>>b;
+    if(op == '+'){
+        cout<<"Sum = "<<a+b;
+    }
+    else if(op == '-'){
+        cout<<"Difference = "<<a-b;
+    }
+    else if(op == '*'){
+        cout<<"Product = "<<a*b;
+    }
+    else if(op == '/'){
+        if(b != 0){
+            cout<<"Quotient = "<<a/b;
+        }
+        else{
+            cout<<"Error!";
+        }
+    }
+    else if(op == '%'){
+        if(b != 0){
+            cout<<"Remainder = "<<a%b;
+        }
+        else{
+            cout<<"Error!";
+        }
+    }
+    else{
+        cout<<"Invalid operator!";
+    }
+}
